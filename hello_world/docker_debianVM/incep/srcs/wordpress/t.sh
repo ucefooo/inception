296d1c10b6fe7214630fe0aaff026446c78a2cd4
youssef oussama <youssama@student.1337.ma>
2023-01-10 18:46:30 +0000

    Delete hello_world directory

--- hello_world/docker_debianVM/incep/srcs/wordpress/t.sh
+++
@@ -1,3 +0,0 @@
-wp config create --dbname="mydb" --dbuser="user" --dbpass="password" --dbhost="mariadb" --path=/var/www/html --allow-root --skip-check
-wp core install  --url="https://4.204.239.60/" --title="ince" --admin_name="admin" --admin_password="SANDala100?" --admin_email="you@gmail.com" --allow-root --path=/var/www/html
-exec "$@"
