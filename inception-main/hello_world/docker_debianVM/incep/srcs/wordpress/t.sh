7d22891ce7e893d6d2e5b98f27bd22b9c586c3d9
youssef oussama <youssama@student.1337.ma>
2023-01-11 00:34:55 +0000

    Add files via upload

--- inception-main/hello_world/docker_debianVM/incep/srcs/wordpress/t.sh
+++ inception-main/hello_world/docker_debianVM/incep/srcs/wordpress/t.sh
@@ -1,3 +1,7 @@
-wp config create --dbname="mydb" --dbuser="user" --dbpass="password" --dbhost="mariadb" --path=/var/www/html --allow-root --skip-check
-wp core install  --url="https://4.204.239.60/" --title="ince" --admin_name="admin" --admin_password="SANDala100?" --admin_email="you@gmail.com" --allow-root --path=/var/www/html
+rm -rf /var/www/html/wp-config.php
+if [ ! -f /var/www/html/wp-config.php ];
+then
+    wp config create --dbname="mydb" --dbuser="user" --dbpass="password" --dbhost="mariadb" --path=/var/www/html --allow-root --skip-check
+    wp core install  --url="https://localhost/" --title="ince" --admin_name="admin" --admin_password="SANDala100?" --admin_email="you@gmail.com" --allow-root --path=/var/www/html
+fi
 exec "$@"
